7763a27ac98e9a86621126453516d03eaaf791b2
Prish20 <prish@students.uonbi.ac.ke>
2024-08-07 23:14:29 +0300

    9. Throw an error

---
+++ 0x01-ES6_promise/9-main.js
@@ -0,0 +1,9 @@
+// 9-main.js
+import guardrail from './9-try.js';
+import divideFunction from './8-try.js';
+
+console.log(guardrail(() => divideFunction(10, 2)));
+// Expected output: [ 5, 'Guardrail was processed' ]
+
+console.log(guardrail(() => divideFunction(10, 0)));
+// Expected output: [ 'cannot divide by 0', 'Guardrail was processed' ]
---
+++ 0x01-ES6_promise/__tests__/9-try.test.js
@@ -0,0 +1,13 @@
+// __tests__/9-try.test.js
+import guardrail from '../9-try.js';
+import divideFunction from '../8-try.js';
+
+test('guardrail should return the result and guardrail message on success', () => {
+  const result = guardrail(() => divideFunction(10, 2));
+  expect(result).toEqual([5, 'Guardrail was processed']);
+});
+
+test('guardrail should return the error message and guardrail message on error', () => {
+  const result = guardrail(() => divideFunction(10, 0));
+  expect(result).toEqual(['cannot divide by 0', 'Guardrail was processed']);
+});
