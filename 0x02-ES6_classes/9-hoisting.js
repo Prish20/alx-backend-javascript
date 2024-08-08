d599594f82d5a978b4427ca48cedf79285d3d0a1
Prish20 <prish@students.uonbi.ac.ke>
2024-08-09 01:58:05 +0300

    Task 9 fix

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -35,11 +35,9 @@ class StudentHolberton {
   }
 }
 
-// Create instances of HolbertonClass
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-// Create instances of StudentHolberton
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
